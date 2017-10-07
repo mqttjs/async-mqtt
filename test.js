a2433d59c4fbc5a78630f02e2b28a2df086a4365
Iman Tabrizian <tabrizian@outlook.com>
2017-10-07 16:10:14 +0330

    use single quotes only
    Unifies the test file to use single quotes only.

--- test.js
+++ test.js
@@ -1,95 +1,95 @@
-"use strict";
+'use strict';
 
-var Server = require("mqtt/test/server");
+var Server = require('mqtt/test/server');
 
-var AsyncMQTT = require("./");
+var AsyncMQTT = require('./');
 var AsyncClient = AsyncMQTT.AsyncClient;
 
-var test = require("tape");
+var test = require('tape');
 
 var SERVER_PORT = 1883;
-var SERVER_URL = "mqtt://localhost:" + SERVER_PORT;
+var SERVER_URL = 'mqtt://localhost:' + SERVER_PORT;
 
 var server = buildServer().listen(SERVER_PORT);
 server.unref();
 
-server.on("listening", runTests);
+server.on('listening', runTests);
 
 function runTests() {
-	test("Connect should return an instance of AsyncClient", function (t) {
+	test('Connect should return an instance of AsyncClient', function (t) {
 		t.plan(1);
 		var client = AsyncMQTT.connect(SERVER_URL);
 
-		t.ok(client instanceof AsyncClient, "Connect returned an AsyncClient");
+		t.ok(client instanceof AsyncClient, 'Connect returned an AsyncClient');
 
 		client.end();
 	});
 
-	test("Should be able to listen on event on client", function (t) {
+	test('Should be able to listen on event on client', function (t) {
 		t.plan(1);
 
 		var client = AsyncMQTT.connect(SERVER_URL);
 
-		client.once("connect", function () {
-			t.pass("Connected");
+		client.once('connect', function () {
+			t.pass('Connected');
 			client.end();
 		});
 	});
 
-	test("Calling end() should resolve once disconnected", function (t) {
+	test('Calling end() should resolve once disconnected', function (t) {
 		t.plan(2);
 
 		var client = AsyncMQTT.connect(SERVER_URL);
 
-		client.on("close", function () {
-			t.pass("Close event occured");
+		client.on('close', function () {
+			t.pass('Close event occured');
 		});
 
-		client.on("connect", function () {
+		client.on('connect', function () {
 			// Wait for connect to emit before ending
 			client.end().then(function(){
-				t.pass("End resolved");
+				t.pass('End resolved');
 			});
 		});
 	});
 
-	test("Calling subscribe should resolve once subscribed", function (t) {
+	test('Calling subscribe should resolve once subscribed', function (t) {
 		t.plan(1);
 
 		var client = AsyncMQTT.connect(SERVER_URL);
 
-		client.subscribe("example", {
+		client.subscribe('example', {
 			qos: 1
 		}).then(function(){
-			t.pass("Subscribed");
+			t.pass('Subscribed');
 			client.end();
 		})
 	});
 
-	test("Calling unsubscribe should resolve once completed", function(t){
+	test('Calling unsubscribe should resolve once completed', function(t){
 		t.plan(1);
 
 		var client = AsyncMQTT.connect(SERVER_URL);
 
-		client.subscribe("example", {
+		client.subscribe('example', {
 			qos: 1
 		}).then(function(){
-			return client.unsubscribe("example");
+			return client.unsubscribe('example');
 		}).then(function(){
-			t.pass("Unsunbscribed");
+			t.pass('Unsunbscribed');
 			return client.end();
 		});
 	});
 
-	test("Calling publish should resolve once completed", function (t) {
+	test('Calling publish should resolve once completed', function (t) {
 		t.plan(1);
 
 		var client = AsyncMQTT.connect(SERVER_URL);
 
-		client.publish("example", "test", {
+		client.publish('example', 'test', {
 			qos: 1
 		}).then(function(){
-			t.pass("Published");
+			t.pass('Published');
 			return client.end();
 		});
 	});
